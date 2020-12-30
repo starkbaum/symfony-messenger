cfb50db138415c36c03464419f239c36662d7275
Stefan Starkbaum <s.starkbaum@austrian-standards.at>
2020-12-30 11:40:57 +0100

    Chapter 35: amqp with rabbitmq

--- src/Controller/ImagePostController.php
+++ src/Controller/ImagePostController.php
@@ -76,7 +76,7 @@ class ImagePostController extends AbstractController
 
         $message = new AddPonkaToImage($imagePost->getId());
         $envelope = new Envelope($message, [
-            new DelayStamp(500),
+            //new DelayStamp(500),
         ]);
         dump($messageBus->dispatch($envelope));
 
