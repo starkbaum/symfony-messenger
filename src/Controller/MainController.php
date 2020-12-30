835bc5d584e6cd78dfa09c181a36ebe90e10dfbc
Stefan Starkbaum <s.starkbaum@austrian-standards.at>
2020-12-30 10:52:58 +0100

    Chapter 32: query bus

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,16 +2,30 @@
 
 namespace App\Controller;
 
+use App\Message\Query\GetTotalImageCount;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/")
+     * @param MessageBusInterface $queryBus
+     * @return Response
      */
-    public function homepage()
+    public function homepage(MessageBusInterface $queryBus)
     {
-        return $this->render('main/homepage.html.twig');
+        $envelope = $queryBus->dispatch(new GetTotalImageCount());
+        /** @var HandledStamp $handled */
+        $handled = $envelope->last(HandledStamp::class);
+
+        $imageCount = $handled->getResult();
+
+        return $this->render('main/homepage.html.twig', [
+            'imageCount' => $imageCount,
+        ]);
     }
 }
